362bccffbd65ca106784f5f72d96a00e58c79b03
otula71 <49908041+otula71@users.noreply.github.com>
2023-04-12 16:36:13 +0200

    Add files via upload

--- Debug.h
+++ Debug.h
@@ -11,4 +11,21 @@
    #define DEBUG_PRINTLN(x)
 #endif
 
+#ifdef DEBUG_SENZORY
+   #define DEBUG_PRINTS(x)     Serial.print(x)
+   #define DEBUG_PRINTSLN(x)   Serial.println(x)
+#else
+   #define DEBUG_PRINTS(x)
+   #define DEBUG_PRINTSLN(x)
+#endif
+
+#ifdef DEBUG_TRIMRY
+   #define DEBUG_PRINTT(x)     Serial.print(x)
+   #define DEBUG_PRINTTLN(x)   Serial.println(x)
+#else
+   #define DEBUG_PRINTT(x)
+   #define DEBUG_PRINTTLN(x)
+#endif
+
+
 #endif
